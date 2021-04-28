a10b6598536ed9d8479caa2f8264eafec210e6d5
Alvaro Gabriel <alvarogabrielgomez@gmail.com>
2021-04-28 15:53:08 -0300

    err handler?

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 require('dotenv').config()
 
 const packageJson = require('./package.json');
@@ -13,6 +14,17 @@ const proxy = httpProxy.createProxyServer({
 
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
     proxyReq.setHeader('X-Accentio-Proxy', `${packageJson.name} v${packageJson.version}`);
+});
+
+proxy.on('error', function(e) {
+    console.error('ProxyError', e);
+
+    res.set({
+        'Cache-Control': 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'
+    });
+    res.writeHead(503, {'content-type': 'text/html'});
+    fs.createReadStream(path.resolve(__dirname, './error.html')).pipe(res);
+    res.end();
 });
 
 const proxyServer = http.createServer(function(req, res) {
